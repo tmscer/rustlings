1d722871675956a9cd23f64c0b7da85cf45e3fe8
Tomáš Černý <tmscer@gmail.com>
2021-02-20 10:09:30 +0100

    complete the enums section

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,25 +1,29 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
-enum Message {
-    // TODO: implement the message variant types based on their usage below
-}
-
 struct Point {
     x: u8,
     y: u8,
 }
 
+#[derive(PartialEq, Debug)]
+struct ColorRGB(u8, u8, u8);
+
+enum Message {
+    Move(Point),
+    Echo(String),
+    ChangeColor(ColorRGB),
+    Quit,
+}
+
 struct State {
-    color: (u8, u8, u8),
+    color: ColorRGB,
     position: Point,
     quit: bool,
 }
 
 impl State {
-    fn change_color(&mut self, color: (u8, u8, u8)) {
+    fn change_color(&mut self, color: ColorRGB) {
         self.color = color;
     }
 
@@ -36,7 +40,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(text) => self.echo(text),
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
@@ -49,14 +58,14 @@ mod tests {
         let mut state = State {
             quit: false,
             position: Point { x: 0, y: 0 },
-            color: (0, 0, 0),
+            color: ColorRGB(0, 0, 0),
         };
-        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::ChangeColor(ColorRGB(255, 0, 255)));
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
 
-        assert_eq!(state.color, (255, 0, 255));
+        assert_eq!(state.color, ColorRGB(255, 0, 255));
         assert_eq!(state.position.x, 10);
         assert_eq!(state.position.y, 15);
         assert_eq!(state.quit, true);
