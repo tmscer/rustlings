22a7af5227f1bbc8ad563a6660883abc62a580bf
Tomáš Černý <tmscer@gmail.com>
2021-02-24 21:32:48 +0100

    complete the tests section

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,10 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!("123", "1".to_owned() + "23");
     }
 }
